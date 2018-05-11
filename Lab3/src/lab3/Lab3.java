a2669c89d757226bfa623a60b509adfbb1f2cc73
trimarchi19 <dtrimarchi19@unitec.edu>
2018-05-11 14:25:41 -0600

    Validaciones del Main para mandar los atributos para la creacion de los Personajes

--- Lab3/src/lab3/Lab3.java
+++ Lab3/src/lab3/Lab3.java
@@ -5,6 +5,9 @@
  */
 package lab3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author X
@@ -16,6 +19,44 @@ public class Lab3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int Ppos=0;//jugador 1-4
+        ArrayList players=new ArrayList<>();
+        Scanner lea=new Scanner(System.in);
+        Scanner lea2=new Scanner (System.in);
+        int opt=1;
+        String color="",Raza="";
+        do{
+        System.out.print("1)Creacion Del Personaje\n"
+                + "2)Creacion Por Default\n"
+                + "Que desea hacer:");
+        opt=lea.nextInt();
+        switch(opt){
+            case 1:
+                color="";
+                Raza="";
+                System.out.println("Ingrese El Nombre de su Personaje");
+                String Nombre=lea2.nextLine();
+                System.out.println("Ingrese la Edad");
+                int edad=lea.nextInt();
+                System.out.println("Ingrese la Estatura");
+                int estatura=lea.nextInt();
+                System.out.println("Ingrese el Peso");
+                int peso=lea.nextInt();
+                do{
+                    System.out.println("Ingrese el Color de Pelo[negro/cafe/rubio]"); 
+                    color=lea2.nextLine().toLowerCase();                   
+                }while(!(color.equals("negro") || color.equals("cafe")||color.equals("rubio")));
+                 do{
+                    System.out.println("Ingrese la Raza [hobbit/elfo/humano/enano]"); 
+                    Raza=lea2.nextLine().toLowerCase();                   
+                }while(!(Raza.equals("hobbit") || Raza.equals("elfo")||Raza.equals("humano")||Raza.equals("enanos")));
+                
+                players.add(new Personajes(edad,Nombre,estatura,peso,Raza));
+                break;
+            default:
+                break;
+        }
+        }while(opt!=5);
     }
     
 }
